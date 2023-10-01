c6ec751fd36bfdf4480184994e48bb069bc67b5d
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-09-30 21:18:30 -0300

    Filtrar pessoas com idade maior que 25

--- elementos_de_um_hash_base_criterio.rb
+++ elementos_de_um_hash_base_criterio.rb
@@ -1,10 +1,11 @@
 pessoa = {
      "Alice" => 25,
-     "joao" => 30,
-     "maria" => 22,
+     "Joao" => 30,
+     "Maria" => 22,
      "entao" => 35
+
 }
 
-#Filtrar pessoas com idade maior que 25
-filtrando_pessoa = pessoa.select { |nome, idade| idade > 25}
+#Filtrando pessoas com idade maior que 25
+filtrando_pessoa = pessoa.select { |nome, idade| idade > 25 }
 puts filtrando_pessoa
