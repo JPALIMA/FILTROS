cd68702dba700c2570e3695c05b23a4c85ec2152
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-09-30 21:30:05 -0300

    comando git add indica que o Git não encontrou o arquivo do projeto, erro no caminho do arquivo ou diretorio, retirado e reefeito o projeto

--- Filtrando_palavras_letra_específica.rb
+++
@@ -1,5 +0,0 @@
-palavras = ["maçã", "banana", "cereja", "morango", "kiwi", "uva"]
-
-#Filtrar palavras que começam com a letra 'b'
-palavras_b = palavras.select { |palavra| palavra.start_with?('b')}
-puts palavras_b
